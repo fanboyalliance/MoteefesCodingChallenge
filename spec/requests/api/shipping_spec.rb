e6c04f1fe3a849bd7da17893a0a05a37afc10d85
fanboyalliance <alex.raswqa@gmail.com>
2021-03-15 01:42:41 +0300

    up swagger doc

--- spec/requests/api/shipping_spec.rb
+++ spec/requests/api/shipping_spec.rb
@@ -2,4 +2,38 @@ require 'swagger_helper'
 
 RSpec.describe 'api/shipping', type: :request do
 
+  path '/api/shipping/search' do
+
+    post('search shipping') do
+      response(200, 'successful') do
+        tags 'Shippings'
+        consumes 'application/json'
+        parameter name: :json, in: :body, schema: {
+          type: :object,
+          properties: {
+            shippingRegion: { type: :string },
+            orderedItems: {
+              type: :array,
+              items: {
+                type: :object,
+                properties: {
+                  itemName: { type: :string },
+                  count: { type: :integer },
+                }},
+
+            }
+          }
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
 end
